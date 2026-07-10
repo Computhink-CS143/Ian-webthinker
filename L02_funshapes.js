566ebc529b98e40f09c34df0cb94ef54fef884b7
Computhink-CS143 <cs143@computhink.com.sg>
2026-07-10 10:41:02 +0000

    Jul 10, 2026, 10:41 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -14,8 +14,6 @@ function draw() {
   // circle(425, 200, 100)
   fill("white");
   rect(200, 200, 100, 75);
-  fill("white");
-  rect(200, 200, 100, 75);
   // Challenge 2: A face using circle() function
   
   // Challenge 3: A house using rect() function
