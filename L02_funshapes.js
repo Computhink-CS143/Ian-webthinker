d4800373dd9c44a9e8fe178dbf126c1a1b51b8be
Computhink-CS143 <cs143@computhink.com.sg>
2026-07-10 10:39:42 +0000

    Jul 10, 2026, 10:39 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -10,10 +10,6 @@ function draw() {
   noStroke();
   // Challenge 1: An eye using ellipse() function
   // fill("white")
-  // ellipse(200, 200, 110, 70);
-  // fill("black")
-  // ellipse(200, 200, 70, 70);
-  // fill("white")
   // circle(300, 200, 150);
   // fill("black")
   // circle(425, 200, 100)
