7da9bb10e09b0e82e6d6c79afb1bc0a68da6c210
Computhink-CS143 <cs143@computhink.com.sg>
2026-07-17 10:54:09 +0000

    Jul 17, 2026, 10:54 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -11,7 +11,7 @@ function draw() {
   // }
   // Task 1: Colour Gradient
   for( let red=0; red<256; red+=35){
-    fill(0, 0, red)
+    fill(0, 0, red);
     rect(50, 50, 50, 50);
   }
   // Task 2: Colour Loop
