3a51ca845f23bf752dba081738716bfbf106c63e
Computhink-CS143 <cs143@computhink.com.sg>
2026-07-17 10:42:54 +0000

    Jul 17, 2026, 10:42 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -7,9 +7,8 @@ function setup() {
 function draw() {
   // Recap 1: Repeating Circles
   for( let circleX=1; circleX<7; circleX++){
-    for( let circleY=1; circleY<7; circleY++){
       circle(circleX*50, circleY*50, 50);
-    }
+  
   }
   // Task 1: Colour Gradient
 
@@ -18,9 +17,9 @@ function draw() {
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
-  for( let circleX=1; circleX<7; circleX++){
-    for( let circleY=1; circleY<7; circleY++){
-      circle(circleX*50, circleY*50, 50);
-    }
-  }
+  // for( let circleX=1; circleX<7; circleX++){
+  //   for( let circleY=1; circleY<7; circleY++){
+  //     circle(circleX*50, circleY*50, 50);
+  //   }
+  // }
 }
