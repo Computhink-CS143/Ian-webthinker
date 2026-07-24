ed090b4ea082da53b31c7c93fa7f015878aa92f0
Computhink-CS143 <cs143@computhink.com.sg>
2026-07-24 10:18:38 +0000

    Jul 24, 2026, 10:18 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -9,14 +9,14 @@ function draw() {
     // print(width);
     // print(height);
     noStroke()
-    let xpos = 25;
-    let ypos = 25;
-    let ggcolour = 0;
-    for(let i = 0; i < 20; i++) {
-        fill(0, ggcolour, 0)
-        circle(xpos, ypos, 25)
-        xpos +=25;
-        ypos +=25;
-        ggcolour +=15;
-    }
+    // let xpos = 25;
+    // let ypos = 25;
+    // let ggcolour = 0;
+    // for(let i = 0; i < 20; i++) {
+    //     fill(0, ggcolour, 0)
+    //     circle(xpos, ypos, 25)
+    //     xpos +=25;
+    //     ypos +=25;
+    //     ggcolour +=15;
+    // }
 }
