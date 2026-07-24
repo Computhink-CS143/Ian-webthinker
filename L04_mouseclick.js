44bb59dc6e21e30d9c9c96986d64d746dfb76ce8
Computhink-CS143 <cs143@computhink.com.sg>
2026-07-24 10:25:06 +0000

    Jul 24, 2026, 10:25 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -24,5 +24,5 @@ function draw() {
     circle (300, 300, 50);
 }
 function mouseReleased() {
-    shapeColour = colour(random(255), random(255), random(255));
+    shapeColour = color(random(255), random(255), random(255));
 }
