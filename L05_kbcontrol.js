9d20b688dcf4f255d9ccbbf3f2944e4c3cdbd0de
Computhink-CS143 <cs143@computhink.com.sg>
2026-07-31 10:15:48 +0000

    Jul 31, 2026, 10:15 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -18,7 +18,7 @@ function draw() {
         rect(width/2, height/2, size, size);
     }
     if (showRect){
-        rect(width/2, height/2, size, size+5);
+        rect(width/4, height/2, size, size+5);
     }
 }
 
