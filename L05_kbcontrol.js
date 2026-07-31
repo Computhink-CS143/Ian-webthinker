cfdeb9d25da1e3e3af870f7a89df2f43b8c7feda
Computhink-CS143 <cs143@computhink.com.sg>
2026-07-31 10:49:03 +0000

    Jul 31, 2026, 10:49 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -35,10 +35,10 @@ function draw() {
         xpos -= 3;
     }
     if(keyIsDown(UP_ARROW)){
-        xpos += 3;
+        ypos += 3;
     }
     if(keyIsDown(DOWN_ARROW)){
-        xpos -= 3;
+        ypos -= 3;
     }
 }
 
