0f012e34c89ea2eb77703fa0395486f0cc80f9f9
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-07 10:33:12 +0000

    Aug 7, 2026, 10:33 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -33,7 +33,7 @@ function draw() {
     if(keyIsDown(UP_ARROW)){
             ypos -= 4
     }
-    xpos = constrain(xpos, 0, 31);
+    xpos = constrain(xpos, 0, 310);
     ypos = constrain(ypos, 0, 310);
 }
 
