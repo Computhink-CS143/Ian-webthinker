ffd2d952d3608ce05773cd8df0643341ef908707
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-07 09:47:52 +0000

    Aug 7, 2026, 9:47 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -4,10 +4,11 @@ function preload() {
 
 function setup() {
     createCanvas(300, 300);
-    background("#eb08e7")
+    background("#eb08e7");
+    rectMod
 }
 
 function draw() {
     background("#eb08e7")
-    
+
 }
