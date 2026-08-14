2478b4ab4cbb06710c429782be042ccb2cc94f63
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-14 10:11:47 +0000

    Aug 14, 2026, 10:11 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,48 +1,48 @@
-let picoImg;
-let soundSFX;
-let picoWidth
+// let picoImg;
+// let soundSFX;
+// let picoWidth
 
-function preload() {
-    picoImg = loadImage('assets/pico-a.png');
-    soundSFX = loadSound('assets/pop.mp3');
-}
+// function preload() {
+//     picoImg = loadImage('assets/pico-a.png');
+//     soundSFX = loadSound('assets/pop.mp3');
+// }
 
-let xpos;
-let ypos;
+// let xpos;
+// let ypos;
 
-function setup() {
-    createCanvas(400, 400);
-    background("#000000");
-    noStroke()
-    xpos = width/2;
-    ypos = height/2;
-    picoWidth = 110
-}
+// function setup() {
+//     createCanvas(400, 400);
+//     background("#000000");
+//     noStroke()
+//     xpos = width/2;
+//     ypos = height/2;
+//     picoWidth = 110
+// }
 
-function draw() {
-    background("#000000");
-    image(picoImg, xpos, ypos, picoWidth, 133)
-    if(keyIsDown(LEFT_ARROW)){
-            xpos -= 4
-    }
-    if(keyIsDown(RIGHT_ARROW)){
-            xpos += 4
-            picoWidth = 110
-    }
-    if(keyIsDown(DOWN_ARROW)){
-            ypos += 4
-            picoWidth = 110
-    }
-    if(keyIsDown(UP_ARROW)){
-            ypos -= 4
-    }
-    xpos = constrain(xpos, 0, width-90);
-    ypos = constrain(ypos, 67, height-67);
-}
+// function draw() {
+//     background("#000000");
+//     image(picoImg, xpos, ypos, picoWidth, 133)
+//     if(keyIsDown(LEFT_ARROW)){
+//             xpos -= 4
+//     }
+//     if(keyIsDown(RIGHT_ARROW)){
+//             xpos += 4
+//             picoWidth = 110
+//     }
+//     if(keyIsDown(DOWN_ARROW)){
+//             ypos += 4
+//             picoWidth = 110
+//     }
+//     if(keyIsDown(UP_ARROW)){
+//             ypos -= 4
+//     }
+//     xpos = constrain(xpos, 0, width-90);
+//     ypos = constrain(ypos, 67, height-67);
+// }
 
-function keyPressed() {
-    if(keyCode === 32) {
-        soundSFX.play();
-        picoWidth = 55
-    }
-}
+// function keyPressed() {
+//     if(keyCode === 32) {
+//         soundSFX.play();
+//         picoWidth = 55
+//     }
+// }
