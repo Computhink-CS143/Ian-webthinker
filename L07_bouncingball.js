5a031bdeefde924f69b9a6eab4a8ead963fed915
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-14 09:50:53 +0000

    Aug 14, 2026, 9:50 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,3 +1,13 @@
+let picoImg;
+let soundSFX;
+let bN;
+
+function preload() {
+    picoImg = loadImage('assets/pico-a.png');
+    soundSFX = loadSound('assets/pop.mp3');
+    bN = loadSound('assets/bossaNova.mp3');
+}
+
 function setup() {
     createCanvas(500, 500);
     background("#000000");
