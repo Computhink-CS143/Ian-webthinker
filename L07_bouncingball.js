67d0df0d278f71a62f0e93745ab342e32ca0d8f4
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-14 09:35:57 +0000

    Aug 14, 2026, 9:35 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,9 +1,9 @@
 function setup() {
     createCanvas(500, 500);
-    background("#bf0cf0");
+    background("#222");
     noStroke()
 }
 
 function draw() {
-    background("#")
+    background("#222")
 }
