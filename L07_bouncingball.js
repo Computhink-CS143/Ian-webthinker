2bcc0e84e7f800abfafcf38d59c70f102db37485
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-14 10:16:29 +0000

    Aug 14, 2026, 10:16 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -47,12 +47,12 @@
 //     }
 // }
 
-let ballX = 300
-let ballY = 200
+let ballX = 300;
+let ballY = 200;
 let ballSize = 30
 let ballSpeedX = 2
 let ballSpeedY = 2
 
 function setup() {
-    createCanvas(600, 400)
+    createCanvas(600, 400);
 }
