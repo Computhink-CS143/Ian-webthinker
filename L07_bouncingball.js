c135420b62555c85120b050155e2bf3baaa7ea2f
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-14 09:51:38 +0000

    Aug 14, 2026, 9:51 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -16,4 +16,27 @@ function setup() {
 
 function draw() {
     background("#000000")
+    image(picoImg, xpos, ypos, 110, 133)
+    if(keyIsDown(LEFT_ARROW)){
+            xpos -= 4
+    }
+    if(keyIsDown(RIGHT_ARROW)){
+            xpos += 4
+    }
+    if(keyIsDown(DOWN_ARROW)){
+            ypos += 4
+            bN.stop();
+    }
+    if(keyIsDown(UP_ARROW)){
+            ypos -= 4
+            bN.loop();
+    }
+    xpos = constrain(xpos, 55, width-55);
+    ypos = constrain(ypos, 67, height-67);
+}
+
+function keyPressed() {
+    if(keyCode === 32) {
+        soundSFX.play();
+    }
 }
