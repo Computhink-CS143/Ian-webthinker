e83b353ff3910dfdf99c86c8ad797c7825fce601
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-21 10:49:22 +0000

    Aug 21, 2026, 10:49 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -32,7 +32,7 @@ function draw() {
     fill("#faff00")
     textSize(24);
     textAlign(CENTER, CENTER);
-    let storytext = ["I am Ian", "I like pizza", ""]
+    let storytext = ["I am Ian", "I like pizza", "pizza...", ""]
     for (let count = 0; count < storytext.length; count++) {
         let sentence = storytext[count]
         text(sentence, width/2, scrollingY+ count * lineGap);
