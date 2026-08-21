9758ec9335099e178b1455254a4a108f1843cd9d
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-21 09:50:18 +0000

    Aug 21, 2026, 9:50 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
     background("#000000")
-    fill("#")
+    fill("#faff00")
     text("my name is Ian", 50, 50);
     text("I am 11 this year", 50, 100);
     text("my favourite activity is playing badminton", 50, 150);
