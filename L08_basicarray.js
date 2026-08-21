57293578bf7e9c397354ff9aae57c6de38d63ed9
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-21 10:27:34 +0000

    Aug 21, 2026, 10:27 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,4 +1,4 @@
-let scrollingY
+let scrollingY = 
 
 function setup() {
     createCanvas(400, 700)
@@ -6,6 +6,7 @@ function setup() {
     noStroke()
     textSize(15)
     fill("#faff00")
+    
     // text("my name is Ian", 50, 50);
     // text("I am 11 this year", 50, 100);
     // text("my favourite activity is playing badminton", 50, 150);
