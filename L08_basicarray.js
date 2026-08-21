c4bbf0e7c87890876641ba5e474af0a6894671e8
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-21 10:11:17 +0000

    Aug 21, 2026, 10:11 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -12,7 +12,7 @@ function setup() {
     for(let count = 0; count < yummyFood.length; count++) {
         // print(yummyFood[count])
         text(yummyFood[count] + " x" + width, 50, ypos);
-        
+        ypos += 50
     }
 }
 
