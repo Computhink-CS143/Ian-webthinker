f9d71fddfb3cbdd6e1cf5ef01efc19026be85d8d
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-21 10:03:05 +0000

    Aug 21, 2026, 10:03 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -12,5 +12,5 @@ function draw() {
     // text("I am 11 this year", 50, 100);
     // text("my favourite activity is playing badminton", 50, 150);
     let ypos = 50
-    let yummyFood = ["ice cream", ""]
+    let yummyFood = ["ice cream", "fried chicken", ""]
 }
