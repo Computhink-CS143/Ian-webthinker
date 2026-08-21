9b8ed5b6ec1e35c1faf032de894a0e88039d2274
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-21 10:58:24 +0000

    Aug 21, 2026, 10:58 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -33,7 +33,7 @@ function draw() {
     fill("#faff00")
     textSize(24);
     textAlign(CENTER, CENTER);
-    let storytext = ["I am Ian", "I like pizza", "pizza...", "PIZZA...", "PIZZA = CHEESE", "I NEED CHEESE", "I want some pizza", "I will go get pizza now", "I also want some fries", "THE END"]
+    let storytext = ["I am Ian", "I like pizza", "pizza...", "PIZZA...", "PIZZA = CHEESE", "I NEED CHEESE", "I want some pizza", "I will go get pizza now", "I also want some fries", "", "THE END"]
     for (let count = 0; count < storytext.length; count++) {
         let sentence = storytext[count]
         text(sentence, width/2, scrollingY+ count * lineGap);
