92057443473b563760efe3361a5923071fff0b18
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-21 10:39:55 +0000

    Aug 21, 2026, 10:39 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,7 +10,7 @@ function setup() {
     // text("my name is Ian", 50, 50);
     // text("I am 11 this year", 50, 100);
     // text("my favourite activity is playing badminton", 50, 150);
-    // 
+    // task 2
     // textAlign(LEFT, TOP)
     // let ypos = 100
     // let yummyFood = ["ice cream", "fried chicken", "burger", "pasta", "pizza"]
