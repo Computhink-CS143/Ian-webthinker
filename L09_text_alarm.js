ab63aa04e93f2ab223a91131845774eeac8809aa
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-28 10:10:49 +0000

    Aug 28, 2026, 10:10 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -9,7 +9,7 @@ function draw() {
     background("#222");
     let xhour = nf( hour(), 2);
     let minute = nf( minute(), 2);
-    let seconds = nf;
+    let seconds = nf( seconds);
     let timeString = "toatally real clock"
 }
 
