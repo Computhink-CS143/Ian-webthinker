593b4c0a192613f59fccb5805271365cad29f546
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-28 10:15:25 +0000

    Aug 28, 2026, 10:15 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -7,7 +7,7 @@ function setup(){
 
 function draw() {
     background("#222");
-    fill("")
+    fill("white")
     let xhour = nf( hour(), 2);
     let xminute = nf( minute(), 2);
     let xseconds = nf( second(), 2);
