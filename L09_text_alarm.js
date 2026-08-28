0941a57ca5c9642a26310d41c398487191d4f631
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-28 10:02:56 +0000

    Aug 28, 2026, 10:02 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,24 +1,24 @@
-let xpos;
-let speed;
+// let xpos;
+// let speed;
 
-function setup() {
-    createCanvas(600, 400);
-    background("#5d5757");
-    textSize(24);
-    textAlign(CENTER, CENTER);
-    xpos = width/2
-    speed = 2
-}
+// function setup() {
+//     createCanvas(600, 400);
+//     background("#5d5757");
+//     textSize(24);
+//     textAlign(CENTER, CENTER);
+//     xpos = width/2
+//     speed = 2
+// }
 
-function draw() {
-    background("#5d5757")
-    text("BOUNCE!", xpos, height/2);
-    xpos = xpos + speed;
+// function draw() {
+//     background("#5d5757")
+//     text("BOUNCE!", xpos, height/2);
+//     xpos = xpos + speed;
 
-    if (xpos > width) {
-        speed = speed * -1
-    }
-    if (xpos < 0){
-        speed = speed * -1
-    }
-}
+//     if (xpos > width) {
+//         speed = speed * -1
+//     }
+//     if (xpos < 0){
+//         speed = speed * -1
+//     }
+// }
