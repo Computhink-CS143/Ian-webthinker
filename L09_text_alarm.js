4fbcceef49d3367d4137dc997e764d06f250aba1
Computhink-CS143 <cs143@computhink.com.sg>
2026-08-28 09:38:13 +0000

    Aug 28, 2026, 9:38 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -5,4 +5,17 @@ function setup() {
 
 function draw() {
     background("#ffffff")
+    fill("#faff00")
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    let storytext = ["I am Ian", "I like pizza", "pizza...", "PIZZA...", "PIZZA = CHEESE", "I NEED CHEESE", "I want some pizza", "I will go get pizza now", "I also want some fries", "IAN ALSO WANT ICE CREAM", "THE END"]
+    for (let count = 0; count < storytext.length; count++) {
+        let sentence = storytext[count]
+        text(sentence, width/2, scrollingY+ count * lineGap);
+    }
+    scrollingY -= 1;
+    console.log(scrollingY);
+    if(scrollingY < -storytext.length * (lineGap/5)) {
+        scrollingY = height
+    }
 }
