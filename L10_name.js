f5951e6cd53c8496fc826c165e41e572aeb36dd0
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 10:50:31 +0000

    Sep 11, 2026, 10:50 AM

--- L10_name.js
+++ L10_name.js
@@ -16,7 +16,7 @@ function setup() {
     ageInput.position(270, 550);
     ageInput.input(updateText);
 
-    colourPicker = createColorPicker("#255")
+    colourPicker = createColorPicker("rgb(255, 255, 255)")
     colourPicker.position(270, 590)
 }
 
