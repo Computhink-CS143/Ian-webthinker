a28afbda61ad4c1e12164c7a1e3429cad6497687
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 10:25:15 +0000

    Sep 11, 2026, 10:25 AM

--- L10_name.js
+++ L10_name.js
@@ -20,7 +20,7 @@ function draw() {
     fill(0);
     textSize(24);
     textAlign(CENTER, CENTER);
-    text(userText, width/2, height/2);
+    text(userText, width/2 + 50, height/2 + 50);
 
     textSize(24);
     textAlign(CENTER, CENTER);
