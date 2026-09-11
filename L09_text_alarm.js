5fa96720d5f524f1e9219149b423990d9f4963dc
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 09:35:42 +0000

    Sep 11, 2026, 9:35 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,3 +1,4 @@
+
 function setup(){
     createCanvas(400, 400);
     background("#222");
@@ -12,7 +13,7 @@ function draw() {
     // let xminute = nf( minute(), 2);
     // let xseconds = nf( second(), 2);
     // let timeString = xhour + ":" + xminute + ":" + xseconds;
-    
+
 
     text(timeString, width/2, height/2)
 }
