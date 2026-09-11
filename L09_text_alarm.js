d47811d8caa26c6b903138721cda6b4842c4d980
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 09:56:31 +0000

    Sep 11, 2026, 9:56 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -22,7 +22,7 @@ function draw() {
     let xminute = nf( minute(), 2);
     let xseconds = nf( second(), 2);
     let timeString = xhour + ":" + xminute + ":" + xseconds;
-    text(timeString, width/2, height/2)
+    text(timeString, 180, height/2)
     textSize(10)
     text("click me to start a timer", 100, 300)
 }
