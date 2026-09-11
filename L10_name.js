ed1280763ed0c692065b7032390185c1219a1e84
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 10:28:03 +0000

    Sep 11, 2026, 10:28 AM

--- L10_name.js
+++ L10_name.js
@@ -1,40 +1,40 @@
 
-let userText = "ENTER YOUR TEXT HERE";
-let input;
-let ageText = "ENTER YOUR AGE";
-let ageInput;
-function setup() {
-    createCanvas(800, 800);
-    background("skyblue");
+// let userText = "ENTER YOUR TEXT HERE";
+// let input;
+// let ageText = "ENTER YOUR AGE";
+// let ageInput;
+// function setup() {
+//     createCanvas(800, 800);
+//     background("skyblue");
 
-    input = createInput("");
-    input.position(160, 520);
-    input.input(updateText);
-    ageInput= createInput("");
-    ageInput.position(160, 550);
-    ageInput.input(updateText);
-}
+//     input = createInput("");
+//     input.position(160, 520);
+//     input.input(updateText);
+//     ageInput= createInput("");
+//     ageInput.position(160, 550);
+//     ageInput.input(updateText);
+// }
 
-function draw() {
-    background("skyblue")
-    fill(0);
-    textSize(24);
-    textAlign(CENTER, CENTER);
-    text(userText, width/2, height/2 - 50);
+// function draw() {
+//     background("skyblue")
+//     fill(0);
+//     textSize(24);
+//     textAlign(CENTER, CENTER);
+//     text(userText, width/2, height/2 - 50);
 
-    textSize(24);
-    textAlign(CENTER, CENTER);
-    text(ageText, width/2, height/2);
+//     textSize(24);
+//     textAlign(CENTER, CENTER);
+//     text(ageText, width/2, height/2);
 
-    textSize(14);
-    textAlign(LEFT);
-    text("Enter name here:", 50, 530)
-    text("Enter age here:", 50, 560)
+//     textSize(14);
+//     textAlign(LEFT);
+//     text("Enter name here:", 50, 530)
+//     text("Enter age here:", 50, 560)
 
-}
-function updateText() {
-    userText = this.value();
-}
-function updateageText() {
-    ageText = this.value();
-}
+// }
+// function updateText() {
+//     userText = this.value();
+// }
+// function updateageText() {
+//     ageText = this.value();
+// }
