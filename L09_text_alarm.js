e04bcf7188ebdfd0e2437434375803a7ee51326e
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 09:53:36 +0000

    Sep 11, 2026, 9:53 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -37,7 +37,7 @@ function countDown() {
         countdown--;
     }else{
         clearInterval(timer)
-        alarmtimer = setInterval(alarmsound, 500);
+        alarmtimer = setInterval(alarm, 500);
     }
 }
 function alarm() {
