574e38db77db98b905248ee814205ee35a34ba0d
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 11:00:00 +0000

    Sep 11, 2026, 11:00 AM

--- L10_name.js
+++ L10_name.js
@@ -21,7 +21,7 @@ function setup() {
     colourPicker.position(270, 590)
 
     rectColourPicker = createColorPicker("rgb(118, 115, 115)")
-    rectColourPicker.position(270, 590)
+    rectColourPicker.position(270, 620)
 }
 
 function draw() {
