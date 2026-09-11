bea68964a9e3694c083db2b1e717a77f1ea6ec7b
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 10:52:18 +0000

    Sep 11, 2026, 10:52 AM

--- L10_name.js
+++ L10_name.js
@@ -21,7 +21,12 @@ function setup() {
 }
 
 function draw() {
-    background("skyblue")
+     background(colourPicker.value())
+    rect(100, 100, 400, 200)
+    textSize(14);
+    textAlign(LEFT);
+    text("change background colour here:", 50, 610)
+
     fill(0);
     textSize(24);
     textAlign(CENTER, CENTER);
