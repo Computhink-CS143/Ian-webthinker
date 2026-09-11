9d100b53b8783a54acbe3abdc0e9184d6b061241
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-11 10:44:46 +0000

    Sep 11, 2026, 10:44 AM

--- L10_name.js
+++ L10_name.js
@@ -43,7 +43,7 @@ let colourPicker;
 function setup() {
     createCanvas(600, 600)
     colourPicker = createColorPicker("#222")
-    colourPicker.position(250, 540)
+    colourPicker.position(250, 560)
 }
 function draw() {
     background(colourPicker.value())
