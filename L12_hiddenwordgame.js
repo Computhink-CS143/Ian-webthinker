028a7841721267d57d55da15d7cc0657c0ba4427
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-25 10:16:20 +0000

    Sep 25, 2026, 10:16 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -8,6 +8,6 @@ function setup(){
 function draw(){
     background("lightblue");
     toryButton = createButton("guess!🤷‍♀️");
-    storyButton.position(width/2+45, 220)
+    storyButton.position(width/2, 220)
     storyButton.mousePressed(updateText)
 }
