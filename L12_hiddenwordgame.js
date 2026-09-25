f26228611c8d00d6dd15a19ea6cbdc7785a505cc
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-25 10:42:55 +0000

    Sep 25, 2026, 10:42 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -3,7 +3,7 @@ let guessInput;
 let wordlist;
 
 let attempts = 0
-let hints
+let hints = "S_"
 
 function setup(){
     wordlist["Angel","Angry","Badge","Baking","Basic","Brave","Bridge","Brief","Broom","Cable","Camel","Candy","Cargo","Chest","Chief","Crown","Cycle","Daily","Dairy","Delay","Desk","Diary","Doubt","Eagle","Elbow","Extra","Faith","False","Fancy","Fault","Final","Flute","Funny","Giant","Glove","Grape","Honey","Index","Input","Joint","Judge","Logic","Lucky","Magic","Major","March","Model","Motor","Mouth","Movie"]
